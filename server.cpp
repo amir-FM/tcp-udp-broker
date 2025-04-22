6437d1de88a2b3678f94bc085b286a5de0fc67a7
Amir Fallah <amir.fallahmirzaei@protonmail.com>
2025-04-22 11:29:25 +0300

    recv udp

--- server.cpp
+++ server.cpp
@@ -19,30 +19,70 @@ public:
 	}
 
 	void start(){
+		start_udp();
+		start_tcp();
+	}
+
+	void stop(){
+		close_udp();
+		close_tcp();
+	}
+
+	int get_tcp_fd(){
+		return tcpfd;
+	}
+	
+	int get_udp_fd(){
+		return udpfd;
+	}
+
+private:
+	int udpfd, tcpfd;
+	struct sockaddr_in serv_addr;
+
+	void start_tcp(){
 		int rc;
 
-		listenfd = socket(AF_INET, SOCK_STREAM, 0);
-		DIE(listenfd < 0, "socket");
+		tcpfd = socket(AF_INET, SOCK_STREAM, 0);
+		DIE(tcpfd < 0, "socket");
 
 		int enable = 1;
-		rc = setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+		rc = setsockopt(tcpfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
 		DIE(rc < 0, "setsockopt");
 
 		set_serv_addr();
 
-		rc = bind(listenfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr));
+		rc = bind(tcpfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
 		DIE(rc < 0, "bind");
 		if(DEBUG)cout << "Started TCP server\n";
 	}
 
-	void stop(){
-		close(listenfd);
+	void close_tcp(){
+		close(tcpfd);
 	}
 
-private:
-	int listenfd;
-	struct sockaddr_in serv_addr;
+	void start_udp(){
+		int rc;
+
+		udpfd = socket(AF_INET, SOCK_DGRAM, 0);
+		DIE(udpfd < 0, "socket");
+
+		int enable = 1;
+		rc = setsockopt(udpfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+		DIE(rc < 0, "setsockopt");
+
+		set_serv_addr();
+
+		rc = bind(udpfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+		DIE(rc < 0, "bind");
+		if(DEBUG)cout << "Started UDP server\n";
+	}
+
+	void close_udp(){
+		close(udpfd);
+	}
 
 	void set_serv_addr(){
 		socklen_t socket_len = sizeof(struct sockaddr_in);
@@ -54,9 +94,36 @@ private:
 	}
 };
 
+class UDP_Connect{
+public:
+	UDP_Connect(int listenfd){
+		this->listenfd = listenfd;
+	}
+
+	int recv_and_print(){
+		struct sockaddr_in cl_addr;
+		socklen_t clen = sizeof(cl_addr);
+
+		int rc = recvfrom(listenfd, packet, sizeof(packet), 0, (struct sockaddr *)&cl_addr, &clen);
+		
+		if(rc > 0)
+			cout << packet << "\n\n";
+
+		return rc;
+	}
+
+private:
+	int listenfd;
+	char packet[1600];
+};
+
 int main(int argc, char *argv[]){
 	Server s(8080);
 	s.start();
 
-	while(1){};
+	UDP_Connect u(s.get_udp_fd());
+
+	while(1){
+		u.recv_and_print();
+	};
 }
